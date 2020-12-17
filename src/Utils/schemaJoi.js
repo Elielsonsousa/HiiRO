1bed9ef6165a929621d09a3d217db12740d0fe9e
Your Name <elielsosousa>
2020-12-16 23:00:40 -0300

    New update3

--- src/Utils/schemaJoi.js
+++ src/Utils/schemaJoi.js
@@ -3,16 +3,14 @@ const Joi = require("@hapi/joi");
 const signUp = Joi.object({   
 
     name: Joi.string()
-        .pattern(/^[a-zA-Z\s.]{2,80}$/)
-        .required(),
+        .pattern(/^[a-zA-Z\s.]{2,80}$/),
 
     email: Joi.string()
         .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'com.br'] } })
         .required(),
-        
+
     password: Joi.string()
-        .pattern(/^[a-zA-Z0-9?!@#$%"&*()_-]{1,32}$/)
-        .required(),
+        .pattern(/^[a-zA-Z0-9?!@#$%"&*()_-]{1,32}$/),
 });
 
 const Update = Joi.object({
